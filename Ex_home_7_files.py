2bd0f181c9b07d8c61b6e0b3c1ef8bf92f7bbef1
Vadim2017 <mr.badik@yandex.ru>
2023-05-15 15:27:34 +0300

    Added Ex_2 in Ex_home_7_files.py

--- Ex_home_7_files.py
+++ Ex_home_7_files.py
@@ -21,3 +21,27 @@ with open('dish_recipes.txt', 'rt') as file:
         cook_book[dishes] = ingredients
 
     pprint(cook_book, sort_dicts=False)
+
+print('\nЗадача №2\n')
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish_name in dishes:
+        for ingredient in cook_book[dish_name]:
+            if ingredient['ingredient_name'] not in shop_list:
+                quantity_dict = {
+                    'measure': ingredient['measure'],
+                    'quantity': int(ingredient['quantity']) * person_count
+
+                }
+                shop_list[ingredient['ingredient_name']] = quantity_dict
+            else:
+                shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * person_count
+
+    pprint(shop_list, sort_dicts=False)
+    print()
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+get_shop_list_by_dishes(['Утка по-пекински', 'Фахитос'], 3)
